20d6e12ce71100e46fce4007386b02fa46693fbd
farhanafiros <farhanafiros541@gmail.com>
2026-08-31 12:21:49 +0530

    Time: 3 ms (61%), Space: 19.3 MB (21.38%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -1,9 +1,9 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        missing_value=[]
-        small_value=min(nums)
-        large_value=max(nums)
-        for num in range(small_value,large_value + 1):
+        missing_value= []
+        maximum =max(nums)
+        minimum =min(nums)
+        for num in range(minimum,maximum+1):
             if num not in nums:
                 missing_value.append(num)
         return missing_value
